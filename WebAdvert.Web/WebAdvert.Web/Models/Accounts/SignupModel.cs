351e1b8d55a9f0f52f75c4f9ccba8c467fff6830
Banuchendhar Singaram <b.singaram@spglobal.com>
2020-03-14 00:11:06 +0530

    First Commit

---
+++ WebAdvert.Web/WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [MinLength(8, ErrorMessage = "Password must be atleast 8 characters long")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password combination doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
